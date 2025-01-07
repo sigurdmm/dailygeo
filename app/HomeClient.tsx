3fba9a542b1d228675d1ad33fd271dfbf577dac2
Sigurd Melsom <sigurd.m.melsom@gmail.com>
2025-01-07 22:04:11 +0100

    Minor UI updates

--- app/HomeClient.tsx
+++ app/HomeClient.tsx
@@ -1,20 +1,35 @@
-'use client'
+"use client";
 
 import { Database } from "@/types/supabase";
-import { Button } from "@nextui-org/react";
+import { Button, Link } from "@nextui-org/react";
+import { PressEvent } from "@react-types/shared";
 
 interface Props {
-    challenge: Database["public"]["Tables"]["challenges"]["Row"]
+  challenge: Database["public"]["Tables"]["challenges"]["Row"];
 }
 
-export default function HomeClient({challenge}: Props) {
+export default function HomeClient({ challenge }: Props) {
+  const challengeUrl = challenge.challenge_token
+    ? `https://www.geoguessr.com/challenge/${challenge.challenge_token}`
+    : undefined;
 
-    const getChallengeUrl = (challenge_token: string) => challenge_token ? `https://www.geoguessr.com/challenge/${challenge_token}` : undefined
-    
-    return (
-        <div className="flex flex-col items-center justify-center w-full h-full">
-            <h1>Daily geo challenges</h1>
-            <h2>{ getChallengeUrl(challenge.challenge_token) ?? "No available challenge"}</h2>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center w-full gap-2">
+      {challengeUrl ? (
+        <Button
+          showAnchorIcon
+          as={Link}
+          color="default"
+          href={challengeUrl}
+          variant="bordered"
+          target="_blank"
+          size="lg"
+        >
+          Open todays geo
+        </Button>
+      ) : (
+        <h2>No content? Just vibes. ✨</h2>
+      )}
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,42 @@
+"use client";
+
+import Link from "next/link";
 import "./globals.css";
 import { Providers } from "./providers";
+import { AiFillGithub } from "react-icons/ai"; // Import GitHub icon from React Icons
+import { Button } from "@nextui-org/react";
+
+const GITHUB_REPO_URL = "https://github.com/sigurdmm/dailygeo";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-      <html lang="en">
-        <body className="antialiased">
-          <div className="min-h-screen">
-          <Providers>
-            {children}
-          </Providers>
-          </div>
-        </body>
-      </html>
+    <html>
+      <body className="antialiased">
+        <div className="min-h-screen flex flex-col">
+          <header className="h-36">
+            <h1 className="flex justify-center items-center text-4xl font-bold h-full">
+              Daily geo challenge
+            </h1>
+          </header>
+          <main className="flex flex-grow flex-col items-center justify-center">
+            <Providers>{children}</Providers>
+          </main>
+          <footer className="flex justify-end items-center text-white p-4">
+            <Button
+              isIconOnly
+              aria-label="GitHub repository"
+              variant="light"
+              as={Link}
+              href={GITHUB_REPO_URL}
+              target="_blank"
+            >
+              <AiFillGithub size={36} color="white" />
+            </Button>
+          </footer>
+        </div>
+      </body>
+    </html>
   );
 }
